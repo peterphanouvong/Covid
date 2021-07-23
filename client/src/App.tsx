b8628ca8ae9e95c4349acdafdf320422d707a5f3
Peter Phanouvong <peterphanouvong@gmail.com>
2021-07-23 10:33:02 +1000

    walking skeleton client

---
+++ client/src/App.tsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Header, Icon, List } from "semantic-ui-react";
+
+function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    const getActivities = async () => {
+      const res = await axios.get("http://localhost:5000/api/activities");
+      console.log(res);
+      setActivities(res.data);
+    };
+
+    getActivities();
+  }, []);
+
+  return (
+    <div className="ui container">
+      <Header as="h2" icon>
+        <Icon name="users" />
+        Activities
+      </Header>
+      <List>
+        {activities.map((activity: any) => (
+          <List.Item>{activity.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
+}
+
+export default App;
